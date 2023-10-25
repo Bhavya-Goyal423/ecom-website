f3639a28f06a2fc728392a21aba147beb812f521
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-25 12:06:38 +0530

    added react-toastify for alerts

--- frontend/src/pages/Signup/SignUp.jsx
+++ frontend/src/pages/Signup/SignUp.jsx
@@ -1,48 +1,100 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const displayError = async (errors, index) => {
+    if (index < errors.length) {
+      toast.error(errors[index]);
+    }
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 500)
+    );
+    await displayError(errors, index + 1);
+  };
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    const data = { name, email, password };
+
+    const res = await fetch("http://localhost:3000/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await res.json();
+    if (!result.success) {
+      displayError(result.message, 0);
+    } else if (result.success) {
+      toast.success("User Created");
+      setName("");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
-    <div className="section-signin">
-      <div className="sign-in-wrapper">
-        <h3 className="get-started">Get Started Now</h3>
-        <p>Enter your details to create your account</p>
-        <form className="sign-in-form">
-          <div className="name-box">
-            <label htmlFor="name">Full Name</label>
-            <input
-              type="text"
-              id="name"
-              className="name"
-              placeholder="Bhavya Goyal"
-            />
-          </div>
-          <div className="email-box">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              className="email-input"
-              placeholder="xyz@gmail.com"
-            />
-          </div>
-          <div className="password-box">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              className="password-input"
-              placeholder="••••••••"
-            />
-          </div>
-          <button className="btn btn-medium btn-signin">Sign Up</button>
-        </form>
-        <p className="sign-up-title">
-          Already have an account?{" "}
-          <Link className="btn-signup" to={"/signin"}>
-            SignIn
-          </Link>
-        </p>
+    <>
+      <div className="section-signin">
+        <div className="sign-in-wrapper">
+          <h3 className="get-started">Get Started Now</h3>
+          <p>Enter your details to create your account</p>
+          <form className="sign-in-form" noValidate onSubmit={handleSumbit}>
+            <div className="name-box">
+              <label htmlFor="name">Full Name</label>
+              <input
+                type="text"
+                id="name"
+                className="name"
+                placeholder="Bhavya Goyal"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="email-box">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="email-input"
+                placeholder="xyz@gmail.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="password-box">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="password-input"
+                placeholder="••••••••"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="btn btn-medium btn-signin">Sign Up</button>
+          </form>
+          <p className="sign-up-title">
+            Already have an account?{" "}
+            <Link className="btn-signup" to={"/signin"}>
+              SignIn
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
