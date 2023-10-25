69a711c21298d29dad2c5e76d8b282e07803974c
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-25 12:05:48 +0530

    added password hasher

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,16 @@
 import UserModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 export default class UserController {
   handleSignUp = async (req, res) => {
     try {
-      const user = await UserModel.create(req.body);
+      let { name, email, password } = req.body;
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+
+      console.log(hash);
+      const data = { name, email, password: hash };
+      const user = await UserModel.create(data);
       return res.json({ success: true, user });
     } catch (error) {
       if (error.message.includes("dup key"))
@@ -11,6 +18,7 @@ export default class UserController {
           success: false,
           message: ["Email already registered"],
         });
+      else console.log(error);
     }
   };
 }
