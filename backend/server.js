16282df6d79293e08e27df0f2c381ab6d721b537
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-25 06:10:36 +0530

    added user schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DB = process.env.DATABASE_URI;
 
 app.listen(3000 || process.env.PORT, () => {
+  mongoose
+    .connect(DB)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   console.log("Server listening at port 3000");
 });
